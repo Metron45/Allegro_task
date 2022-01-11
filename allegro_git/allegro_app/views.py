bfe73f0435bdae3f7733c5faeabcf2dbcdcf8533
Metron45 <metron45@gmail.com>
2022-01-11 13:09:21 +0100

    Slight refractor

--- allegro_git/allegro_app/views.py
+++ allegro_git/allegro_app/views.py
@@ -8,10 +8,18 @@ def repos(request, user):
     r = requests.get('https://api.github.com/users/' + user + '/repos')
     json_response = r.json()
 
+    response, star_sum, lang_dict = list_repositories(json_response)
+    response += list_languages(lang_dict)
+    response += "<br/> Sum of Stars: " + str(star_sum) + "<br/>"
+
+    return HttpResponse(response)
+
+
+def list_repositories(git_response):
     response = ""
     star_sum = 0
     lang_dict = {}
-    for repo in json_response:
+    for repo in git_response:
         response += repo["name"] + " " + str(repo["stargazers_count"]) + " "
         star_sum += repo["stargazers_count"]
         if repo["language"] is not None:
@@ -22,9 +30,12 @@ def repos(request, user):
                 lang_dict[repo["language"]] = repo["size"]
         response += " " + str(repo["size"])
         response += "<br/>"
-
     response += "<br/>"
+    return response, star_sum, lang_dict
 
+
+def list_languages(lang_dict):
+    response = ""
     if len(lang_dict) >= 3:
         for _ in range(0, 3):
             favourite = max(lang_dict, key=lang_dict.get)
@@ -35,7 +46,4 @@ def repos(request, user):
             favourite = max(lang_dict, key=lang_dict.get)
             response += favourite + " " + str(lang_dict[favourite]) + "<br/>"
             lang_dict.pop(favourite)
-
-    response += "<br/> Sum of Stars: " + str(star_sum) + "<br/>"
-
-    return HttpResponse(response)
+    return response
